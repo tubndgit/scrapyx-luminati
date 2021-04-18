488160af246eeb9b9d647d55a77cfff8427af34c
tubndgit <tubnd.younet@gmail.com>
2021-04-18 17:47:35 +0700

    Update script

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='scrapyx-luminati',
-        version='0.1.4',        
+        version='0.1.5',        
         description='Luminati middleware for Scrapy',
         long_description=long_description,
         long_description_content_type='text/markdown',
